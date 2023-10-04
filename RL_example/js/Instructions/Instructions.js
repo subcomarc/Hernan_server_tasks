42f0f1f5fd4cdcf167f1dbbdcc74f9bea9f99f38
subcomarc <subcomarc@gmail.com>
2023-10-04 14:13:15 +0200

    text formatting

--- RL_example/js/Instructions/Instructions.js
+++ RL_example/js/Instructions/Instructions.js
@@ -3,8 +3,8 @@ import {Instructions} from "./dInstructions.js";
 var Welcome = new Instructions(
     {nextText: "Next",
     textInstructions:[//page 0
-                      ["<h1> Welcome to our little task! </h1> <br>",
-                      "<h3> Thank you for choosing to take part in this study. We're happy to have you! </h3> <br><br>",
+                      ["<h1> Welcome to our little task! </h1> <h2><br><br></h2>",
+                      "<h3> Thank you for choosing to take part in this study. We're happy to have you! </h3> <br>",
                       "<h3> With your help, we will try to understand a bit better how people learn new things and make decisions.</h3>"],
 
                       ]  })
