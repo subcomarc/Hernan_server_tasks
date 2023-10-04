fd307a1f8e44c099dde561ab4b1873cb8718eedc
subcomarc <subcomarc@gmail.com>
2023-10-04 17:30:20 +0200

    image still in progress

--- RL_example/js/Instructions/Instructions.js
+++ RL_example/js/Instructions/Instructions.js
@@ -16,9 +16,9 @@ var InstructionsLT = new Instructions(
                       ["<h2>Instructions</h2>",
                       "This study consists of two point-and-click games, in which you will have to choose one out of two symbols displayed on screen. <br></br>",
                       "Here, this is an example of what the symbols can look like:<br>", 
-                      '<img src="images/stim2/17.png" style="padding: 1rem;  width= "155"; height= "155"; class = "img-fluid">  ',
-                      '<img src="images/stim2/18.png" style="padding: 1rem;  width= "155"; height= "155"; class = "img-fluid">  ',
-                      '<img src="images/stim2/19.png" style="padding: 1rem;  width= "155"; height= "155"; class = "img-fluid">  ',
+                      '<img src="images/stim2/17.png" style="padding: 1rem; text-align: center"; width= "155"; height= "155"; class = "img-fluid">  ',
+                      '<img src="images/stim2/18.png" style="padding: 1rem; text-align: center"; width= "155"; height= "155"; class = "img-fluid">  ',
+                      '<img src="images/stim2/19.png" style="padding: 1rem; text-align: center"; width= "155"; height= "155"; class = "img-fluid">  ',
                       "<b>Every symbol has a probability of giving you a reward, you want to accumulate as many points as you can!</b>",
                     ],
 
