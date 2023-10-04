eb07be6e5418036bab37803c456f2bac8acaeafa
subcomarc <subcomarc@gmail.com>
2023-10-04 13:51:30 +0200

    formatting

--- RL_example/js/Instructions/Instructions.js
+++ RL_example/js/Instructions/Instructions.js
@@ -5,7 +5,7 @@ var Welcome = new Instructions(
     textInstructions:[//page 0
                       ["<h1> Welcome to our little task! </h1> <br>",
                       "<h2> Thank you for choosing to take part in this study. We're happy to have you! </h2> ",
-                      "<h3>With your help, we will try to understand a bit better how people learn new things and make decisions. </h3> <h1><br></h1>"],
+                      "<h3> With your help, we will try to understand a bit better how people learn new things and make decisions.</h3> <h1> <br></h1>"],
 
                       ]  })
 
