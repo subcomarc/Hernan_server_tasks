4f72186a0f8b9db4306a986beb925202d58a9cbe
subcomarc <subcomarc@gmail.com>
2023-10-05 16:37:49 +0200

    final touches, but alpha done

--- RL_example/js/Instructions/Instructions.js
+++ RL_example/js/Instructions/Instructions.js
@@ -96,9 +96,9 @@ var InstructionsPreference = new Instructions(
       textInstructions:[//page 1
       ["<h2>Teaching task</h2>",
       "Thank you for going over the two point-and-click tasks. "+
-      "Now, the time has come for you to transmit your strategies to a future player whe shall consider your pupil. " ,
-      "Remember, the options they will have to choose from will have the same value, but they will be represented by different symbols, so focus on the concepts and strategies rather than one concrete symbol's shape or color, since they wont be the same anyways. ",
-      "You really want your pupil to succeed! After all, remember you'll be making 10% of their earnings as an extra bonus."+
+      "<b>Now, the time has come for you to transmit your strategies to a future player whe shall consider your pupil.</b> " ,
+      "Remember, the options they will have to choose from will have the same value, but they will be represented by different symbols, so <b>focus on the concepts and strategies rather than one concrete symbol's shape or color</b>, since they wont be the same anyways. ",
+      "You really want your pupil to succeed! After all, remember <b>you'll be making 10% of their earnings as an extra bonus.</b>"+
       "It may be better to keep your written lesson brief and to the point, but no strict limit for the text is enforced.",
       "<br><br> Click the button below to start."]]
     })
