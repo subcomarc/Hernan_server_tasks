856385e8b773f76e715be85f872c3b962d2fdf51
subcomarc <subcomarc@gmail.com>
2023-11-30 11:23:07 +0100

    Updating teaching

--- RL_example/js/order.js
+++ RL_example/js/order.js
@@ -10,7 +10,9 @@ import {TEACH_PreferenceTask} from "./tasks/TEACH_PreferenceTask.js";
 
 // InstructionsLT
 // import {InstructionsLT,InstructionsLT2,InstructionsLT_Train, InstructionsPreference, InstructionsProbSlider,Welcome} from "./Instructions/Instructions.js"
-import {InstructionsLT, InstructionsLT2, InstructionsLT_Train, InstructionsPreference, InstructionsWriteLesson, Welcome} from "./Instructions/Instructions.js"
+// import {InstructionsLT, InstructionsLT2, InstructionsLT_Train, InstructionsPreference, InstructionsWriteLesson, Welcome} from "./Instructions/Instructions.js"
+import {InstructionsLT, InstructionsLT2, InstructionsWriteLesson, Welcome} from "./Instructions/Instructions.js"
+
 
 // other components
 import {getID} from "./components/getID.js";
@@ -18,7 +20,7 @@ import {end} from "./components/FinalScreen.js";
 import {consentForm} from "./components/dExperimentConsent.js";
 // import {getFeedback} from "./components/Feedback.js";
 import {getWrittenLesson} from "./components/WriteLesson.js";
-// import {Quiz} from "./components/Quiz.js";
+import {Quiz} from "./components/Quiz.js";
 
 // other functions
 import {disableF5} from "./functions/usefulFunctions.js";
@@ -26,9 +28,6 @@ import {disableF5} from "./functions/usefulFunctions.js";
 function expOrder (){
   let check = exp.expTask;
   switch (check) {
-
-
-
     case 0:
       Welcome.init()
       //exp.expTask++
@@ -43,33 +42,33 @@ function expOrder (){
     case 3:
       InstructionsLT.init();
       break;
-    // case 4:
-    //   Quiz.init();;
-    //   break;
     case 4:
-      InstructionsLT_Train.init();
-      break;
+       Quiz.init();;
+       break;
+    // case 4:
+    //   InstructionsLT_Train.init();
+    //   break;
+    // case 5:
+    //   TEACH_LearningTraining.init() ;
+    //   exp.startTimeLTTrain = Date.now();
+    //   break;
     case 5:
-      TEACH_LearningTraining.init() ;
-      exp.startTimeLTTrain = Date.now();
-      break;
-    case 6:
       exp.startTimeInsLT = Date.now();
       InstructionsLT2.init();
       break;
-    case 7:
+    case 6:
       TEACH_LearningTask.init() ;
       exp.startTimeLT = Date.now();
       break;
-    case 8:
-      InstructionsPreference.init();
-      exp.startTimeInsPref = Date.now();
-      break;
-    case 9:
-      TEACH_PreferenceTask.init() ;
-      exp.startTimePref = Date.now();
-      break;
-    case 10:
+    // case 7:
+    //   InstructionsPreference.init();
+    //   exp.startTimeInsPref = Date.now();
+    //   break;
+    // case 8:
+    //   TEACH_PreferenceTask.init() ;
+    //   exp.startTimePref = Date.now();
+    //   break;
+    case 7:
       InstructionsWriteLesson.init();
        exp.startTimeInsPref = Date.now();
        break;
@@ -77,11 +76,11 @@ function expOrder (){
     //   BR_VSlider.init() ;
     //   exp.startTimePref = Date.now();
     //   break;
-    case 11:
+    case 8:
       getWrittenLesson(exp)
       exp.startTimeWrittenLesson = Date.now();
       break;
-   case 12:
+   case 9:
       end(exp);
       exp.startTimeFinal = Date.now();
       break;
