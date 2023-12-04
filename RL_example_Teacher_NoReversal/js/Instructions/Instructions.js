25513f41010a47755063eafc4dacec69219d137e
subcomarc <subcomarc@gmail.com>
2023-12-04 16:57:03 +0100

    final typos

--- RL_example_Teacher_NoReversal/js/Instructions/Instructions.js
+++ RL_example_Teacher_NoReversal/js/Instructions/Instructions.js
@@ -53,7 +53,7 @@ var InstructionsLTp1 = new Instructions(
                      "At the end of the study, we will convert all collected points into pounds and add them to the fixed bonus provided by Prolific. ",
                      "The conversion rate is 1 point = 1.2 pence. ",
                      "<b>But since you will be a teacher, your bonus earnings will not depend on the points you earn, but rather on the points your pupil earns </b>",
-                     "It is therefore in your best interest that you explain the task in the best possible way. If your pupil is good, it could double your earnings! "],
+                     "It is therefore in your best interest that you explain the task in the best possible way. If your pupil is good, you could double your earnings! "],
 
                     ]
   })
@@ -99,10 +99,10 @@ var InstructionsPreference = new Instructions(
       textInstructions:[//page 1
       ["<h2>Teaching task</h2>",
       "Thank you for going over the point-and-click tasks. "+
-      "<b>Now, the time has come for you to transmit your strategies to yout future pupil.</b> " ,
+      "<b>Now, the time has come for you to transmit your strategies to your future pupil.</b> " ,
       '<img src="images/instructions/Teacher_ready.png" style="padding: 1rem; text-align: center" class = "img-fluid">',
       "Remember, they wont have access to any instructions from us. <b>ALL INSTRUCTIONS WILL COME FROM YOU.</b> ",
-      "Also important, the options they will have to choose from will have the same value a yours, but they will be represented by different symbols, so <b>focus on the concepts and strategies rather than one concrete symbol's shape or color</b>, since they wont be the same anyways. ",
+      "Also important, the options they will have to choose from will have the same value as yours, but they will be represented by different symbols, so <b>focus on the concepts and strategies rather than one concrete symbol's shape or color</b>, since they wont be the same anyways. ",
       "You really want your pupil to succeed! After all, remember <b>your extra bonus depends on their performance. </b>"+
       "Also remember that a good teaching text is at least 250 characters, <b>but of course you can extend yourself as much as you want.</b> Don't hesitate to share tips, strategies or any other piece of information that you think will help your pupil. ",
       "<br><br> Click the button below to start."]]
