630b29846a18a3392298745fdbfd7a074bdf22f3
subcomarc <subcomarc@gmail.com>
2023-12-07 10:33:45 +0100

    test

--- RL_example_ICM_Reversal/js/tasks/BREATH_RL_LearningTask.js
+++ RL_example_ICM_Reversal/js/tasks/BREATH_RL_LearningTask.js
@@ -133,12 +133,12 @@ if(track.isreversed == 0){
   ts.reversal == track.symbolA.counter ? track.isreversed=1 : track.isreversed=0;
   ts.reversal == track.symbolA.counter ? track.symbolA.prob=track.symbolA.probR : track.symbolA.prob=track.symbolA.probS;
   ts.reversal == track.symbolA.counter ? track.symbolB.prob=track.symbolB.probR : track.symbolB.prob=track.symbolB.probS;
-  ts.reversal == track.symbolA.counter ? track.symbolA.counter=0 : track.symbolA.counter=track.symbolA.counter
+  ts.reversal == track.symbolA.counter ? track.symbolA.counter=0 : track.symbolA.counter=track.symbolA.counter;
   }else{
   ts.reversal == track.symbolA.counter ? track.isreversed=0 : track.isreversed=1;
   ts.reversal == track.symbolA.counter ? track.symbolA.prob=track.symbolA.probS : track.symbolA.prob=track.symbolA.probR;
   ts.reversal == track.symbolA.counter ? track.symbolB.prob=track.symbolB.probS : track.symbolB.prob=track.symbolB.probR;
-  ts.reversal == track.symbolA.counter ? track.symbolA.counter=0 : track.symbolA.counter=track.symbolA.counter
+  ts.reversal == track.symbolA.counter ? track.symbolA.counter=0 : track.symbolA.counter=track.symbolA.counter;
   }
 // //update tracking of SYMBOLS
 // if(symbols[track.pair[0]].track>=symbols[track.pair[0]].outSchedule.length){
