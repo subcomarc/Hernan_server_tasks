ee1b33d505187953586edf04d64e65d9860dcf74
subcomarc <subcomarc@gmail.com>
2023-12-07 16:07:02 +0100

    act

--- RL_example_ICM_Reversal/php/InsertDB_IDstart.php
+++ RL_example_ICM_Reversal/php/InsertDB_IDstart.php
@@ -8,7 +8,7 @@ $EXPID 		= stripslashes(htmlspecialchars($_POST['expID']));
 
 
 $stmt = $db->prepare("INSERT INTO BREATH_RL_IDstart VALUE(?,?,?, NOW())");
-$stmt->bind_param("sss",
+$stmt->bind_param("its",
     $PARTID,$PARTSEX,$EXPID
     );
 $stmt->execute();
