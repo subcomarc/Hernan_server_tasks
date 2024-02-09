f00ba873d6ec060af2521fe458926576f386958a
subcomarc <subcomarc@gmail.com>
2024-02-09 16:49:14 +0100

    updateIcons

--- RL_example_Teacher_NoReversal/js/tasks/TEACH_LoadSymbols.js
+++ RL_example_Teacher_NoReversal/js/tasks/TEACH_LoadSymbols.js
@@ -23,7 +23,8 @@ var symbols = [
 {id:7,reward:-10,prob:0.25,loss:0}
 ];
 
-addSymImage(symbols,0,"images/stim/",'.gif')
+// addSymImage(symbols,0,"images/stim/",'.gif')
+addSymImage(symbols,0,"images/stim/Exp1",'.gif')
 
 
 // SYMBOLS FOR TRAINING
