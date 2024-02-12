e7fd33f661e77ce8a392c6ddb8eea0b61a2b0f30
subcomarc <subcomarc@gmail.com>
2024-02-12 15:56:24 +0100

    change prolific site

--- RL_example_ICM_Reversal/js/BREATH_RL_ExpSetting.js
+++ RL_example_ICM_Reversal/js/BREATH_RL_ExpSetting.js
@@ -1,7 +1,8 @@
 var exp = {
   expID: 'ICM_RL_Reversal',
   testMode: 'YES',
-  link: "https://app.prolific.co/submissions/complete?cc=CCLR3AVT",
+  // link: "https://app.prolific.co/submissions/complete?cc=CCLR3AVT",
+  link: "https://institutducerveau-icm.org/fr/",
   order: 0,
   rate: 0.012,
   expTask: 0,
