2055f977ad7f314a4d602c9b03fc01f8dfac9bb4
subcomarc <subcomarc@gmail.com>
2024-02-13 13:21:05 +0100

    flowing

--- RL_example_ICM_Reversal/js/order.js
+++ RL_example_ICM_Reversal/js/order.js
@@ -56,7 +56,7 @@ async function expOrder (){
         InstructionsStartDyspnoea.init();
         exp.startTimeInsPref = Date.now();
         break;
-      }else{exp.expTask=exp.expTask+2;break}
+      }else{exp.startTimeInsLT = Date.now(); InstructionsLT2.init(); exp.expTask=exp.expTask+3;break}
      case 7:
       if(exp.Group == 'V'){
         LaunchDyspnoea(exp)
