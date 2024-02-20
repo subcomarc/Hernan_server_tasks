106cca38a9da77da237bd523309c886e52f5bb32
subcomarc <subcomarc@gmail.com>
2024-02-20 17:15:10 +0100

    sss

--- RL_example_ICM_Reversal/js/components/localServerPythonClick.py
+++ RL_example_ICM_Reversal/js/components/localServerPythonClick.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 import pyautogui
 
@@ -13,13 +13,16 @@ def click():
     # Get x and y coordinates from request arguments
     x = request.args.get('x', default=100, type=int)
     y = request.args.get('y', default=100, type=int)
+    clicktest = 0
 
      # Perform three consecutive mouse clicks at (x, y)
     for _ in range(3):
         pyautogui.click(x, y)
+        clicktest = clicktest+1
 
     # response.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:5000'
-    return jsonify(success=True, message=f"Clicked at ({x}, {y})")
+    return jsonify(success=True, message=f"Clicked at ({x}, {y})", clicktest=clicktest)
+
 
 if __name__ == '__main__':
     CLICKapp.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
