284561ce7b7975fcd6dbb2aa59b36d949cccf0e1
subcomarc <subcomarc@gmail.com>
2024-02-28 14:04:29 +0100

    dd

--- RL_example_ICM_Reversal/js/BREATH_RL_ExpSetting.js
+++ RL_example_ICM_Reversal/js/BREATH_RL_ExpSetting.js
@@ -1,8 +1,9 @@
 var exp = {
   expID: 'ICM_RL_Reversal',
-  testMode: 'YES',
+  testMode: 'NO',
   // link: "https://app.prolific.co/submissions/complete?cc=CCLR3AVT",
-  link: "https://institutducerveau-icm.org/fr/",
+  // link: "https://institutducerveau-icm.org/fr/",
+  link: "https://blank.org/",
   order: 0,
   rate: 0.033,
   expTask: 0,
