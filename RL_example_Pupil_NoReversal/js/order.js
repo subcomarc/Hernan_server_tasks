3497f4e65f936f29d4a5078bf11f93f0b9acc30e
subcomarc <subcomarc@gmail.com>
2024-04-09 19:05:29 +0200

    s

--- RL_example_Pupil_NoReversal/js/order.js
+++ RL_example_Pupil_NoReversal/js/order.js
@@ -1,21 +1,12 @@
 // exp - shared data
 import {exp} from "./PUPIL_ExpSetting.js"
-import {initializeInstructions} from './Instructions/Instructions_Lesson.js'; // Assuming this is the correct path
+import {initializeInstructions} from './Instructions/Instructions_Lesson.js'; // This is where we bring the teacher's lesson (which is a promise)
 
 
 // tasks
 import {PUPIL_LearningTask} from "./tasks/PUPIL_LearningTask.js";
-// import {TEACH_LearningTraining} from "./tasks/TEACH_LearningTraining.js";
-// import {TEACH_PreferenceTask} from "./tasks/TEACH_PreferenceTask.js";
 import {PUPIL_VButtons2} from "./tasks/PUPIL_VButtons2.js";
-
-//before we can continue exporting, we need to fecth the teacher's lessons
-
-// InstructionsLT
-// import {InstructionsLT,InstructionsLT2,InstructionsLT_Train, InstructionsPreference, InstructionsProbSlider,Welcome} from "./Instructions/Instructions.js"
-// import {InstructionsLT, InstructionsLT2, InstructionsLT_Train, InstructionsPreference, InstructionsWriteLesson, Welcome} from "./Instructions/Instructions.js"
 import {InstructionsLTp1, InstructionsLTp2, InstructionsLT2, InstructionsProbSlider, Welcome} from "./Instructions/Instructions.js"
-
 
 // other components
 import {getID} from "./components/getID.js";
@@ -42,23 +33,19 @@ async function expOrder (){
     case 3:
       InstructionsLTp1.init();
       break;
-    // case 4:
-    //    Quiz_RL.init();;
-    //    break;
-    // case 4:
-    //   InstructionsLT_Train.init();
-    //   break;
-    // case 5:
-    //   TEACH_LearningTraining.init() ;
-    //   exp.startTimeLTTrain = Date.now();
-    //   break;
     case 4:
       InstructionsLTp2.init();
       break;
     case 5:
-      // Use initializeInstructions() to get Instructions_FROMTEACHER
+      // Use initializeInstructions() to get Instructions_FROMTEACHER, time to satisfy the promises AND THEN initialize the instructions
       const instructions_FROMTEACHER = await initializeInstructions();
       instructions_FROMTEACHER.init();
+      //I'll disable the Next button for a little while here to avoid the user clicking it before the instructions are displayed
+      const nextButton = document.getElementById('bNext');
+       nextButton.disabled = true;
+        setTimeout(() => {
+          nextButton.disabled = false;
+          }, 5000); // 5000 milliseconds = 5 seconds
       break;
     case 6:
       exp.startTimeInsLT = Date.now();
@@ -68,22 +55,6 @@ async function expOrder (){
       PUPIL_LearningTask.init() ;
       exp.startTimeLT = Date.now();
       break;
-    // case 7:
-    //   InstructionsPreference.init();
-    //   exp.startTimeInsPref = Date.now();
-    //   break;
-    // case 8:
-    //   TEACH_PreferenceTask.init() ;
-    //   exp.startTimePref = Date.now();
-    //   break;
-    // case 9:
-    //     InstructionsWriteLesson.init();
-    //      exp.startTimeInsPref = Date.now();
-    //      break;
-    // case 10:
-    //   getWrittenLesson(exp)
-    //   exp.startTimeWrittenLesson = Date.now();
-    //   break;
       case 8:
         InstructionsProbSlider.init();
          exp.startTimeInsPref = Date.now();
