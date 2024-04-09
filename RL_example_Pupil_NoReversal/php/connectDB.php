3dc1f485c9ae2787a64c66a56d205a05108b2b26
subcomarc <subcomarc@gmail.com>
2024-04-09 10:56:26 +0200

    lesson

--- RL_example_Pupil_NoReversal/php/connectDB.php
+++ RL_example_Pupil_NoReversal/php/connectDB.php
@@ -2,10 +2,11 @@
 
 
 
-$database="risc_human_rl";
-$host="mysql-risc.alwaysdata.net";
-$user="risc_human_rl";
-$password="PXyMGjq9QE";
+$database='dec_lnc2_hernananllo';
+$host='mysql-dec.alwaysdata.net';
+$user='dec_palminteri';
+// $password="PXyMGjq9QE";
+$password='Zes;,3DHSy2mQNs';
 
 $db = new mysqli($host, $user, $password, $database);
 
