0f50f1ddf9b5fa9f9baf143a61bd92be142d7374
subcomarc <subcomarc@gmail.com>
2024-04-11 18:26:05 +0200

    LAUNCH

--- RL_example_Teacher_NoReversal/js/TEACH_ExpSetting.js
+++ RL_example_Teacher_NoReversal/js/TEACH_ExpSetting.js
@@ -2,7 +2,7 @@ var exp = {
   expID: 'TEACH_RL_TEACHER_NoReversal',
   // testMode: 'YES',
   testMode: 'NO',
-  link: "https://app.prolific.co/submissions/complete?cc=CCLR3AVT",
+  link: "https://app.prolific.com/submissions/complete?cc=CLM2O650",
   order: 0,
   rate: 0.012,
   expTask: 0,
